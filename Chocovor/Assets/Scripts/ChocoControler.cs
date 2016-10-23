fb614fed47642e8d805ce595b56242b7ad062af5
Michael Eusebe <michael.eusebe@etu.unice.fr>
2016-10-23 18:19:00 +0200

    first commit moving object :D

---
+++ Chocovor/Assets/Scripts/ChocoControler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChocoControler : MonoBehaviour {
+
+    [SerializeField]
+    private int modality = 0;
+
+    [SerializeField]
+    private GameObject Choco;
+
+    private Movement moveComponent;
+
+    // Use this for initialization
+    void Start () {
+        moveComponent = Choco.GetComponent<Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalTranslation = Input.GetAxis("Horizontal");
+        float verticalTranslation = Input.GetAxis("Vertical");
+
+        moveComponent.AddMovement(horizontalTranslation, verticalTranslation);
+    }
+}
---
+++ Chocovor/Assets/Scripts/ChocolatMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChocolatMovement : MonoBehaviour {
+
+    /// <summary>
+    /// horizontalSpeedM
+    /// </summary>
+    [SerializeField]
+    private float horizontalForceMultiplier = 10;
+
+    [SerializeField]
+    private float verticalForceMultiplier = 10;
+
+    [SerializeField]
+    private Vector2 force;
+
+    // Use this for initialization
+    void Start()
+    {
+        force = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(force);
+    }
+
+    public void AddMovement(float horiForce, float vertForce)
+    {
+        force.x = horiForce * horizontalForceMultiplier;
+        force.y = vertForce * verticalForceMultiplier;
+    }
+}
